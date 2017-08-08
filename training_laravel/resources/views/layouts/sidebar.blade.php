51f9fc1159fbc452a5345a5df6447b774ab75369
shunsugiyama <shun909@gmail.com>
2017-08-08 16:19:48 +0800

    Created Home page

---
+++ training_laravel/resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,12 @@
+<div class="container">
+	<nav class="col-sm-3 col-md-2 hidden-xs-down bg-faded sidebar">
+          <ul class="nav nav-pills flex-column">
+            <li class="nav-item">
+              <a class="nav-link active" href="#">Profile<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Learned : 999999999 words</a>
+            </li>
+          </ul>
+        </nav>
+</div>
